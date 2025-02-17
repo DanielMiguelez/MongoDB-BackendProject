39d817cc795f4213eb9ecc675a79b681687a0a03
DanielMiguelez <danielmiguelez1993@gmail.com>
2025-02-17 18:53:04 +0100

    orders updated

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,20 +1,34 @@
-const Order = require ("../models/Order");
+const Order = require("../models/Order");
 
 const OrderController = {
 
-    async createorder (req, res){
+    async createorder(req, res) {
         try {
             const order = await Order.create({
                 ...req.body,
 
-                status:"pending",
-                deliveryDate: new Date().setDate(new Date().getDate() +2),
-                userId : req.user._id,
+                status: "pending",
+                deliveryDate: new Date().setDate(new Date().getDate() + 2),
+                userId: req.user._id,
             })
-            res.status(201).send({msg:"order created", order})
+            res.status(201).send({ msg: "order created", order })
         } catch (error) {
             console.error(error);
-            res.status(500).send({msg:"error al crear la orden"});
+            res.status(500).send({ msg: "error al crear la orden" });
+        }
+    },
+
+    async updateorder(req, res) {
+        try {
+            const orderUpdated = await Order.findByIdAndUpdate(
+                req.params._id,
+                { ...req.body, userId: req.user._id },
+                {new: true,}
+            );
+            res.status(200).send({msg:"order updated",orderUpdated})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ msg: "error al crear la orden" });
         }
     }
 }
--- routes/orders.js
+++ routes/orders.js
@@ -5,6 +5,6 @@ const {authentication}=require("../middlewares/authentication")
 const OrderController = require("../controllers/OrderController")
 
 router.post("/createOrder",authentication, OrderController.createorder)
-
+router.put("/updateOrder/:_id",authentication, OrderController.updateorder)
 
 module.exports = router;
